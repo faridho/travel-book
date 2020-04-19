55bb1cd679cf5e13bb34518ea4f07421b346cca2
Faridho <faridho@zeepos.com>
2020-04-19 12:01:53 +0700

    Initial Commit

---
+++ TravelBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelBook
+//
+//  Created by Faridho on 19/04/20.
+//  Copyright © 2020 Faridho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
